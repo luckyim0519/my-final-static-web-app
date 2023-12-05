e95be360ae1f9cbef96b008a1480461de38ec582
luckyim <moon990519@gmail.com>
2023-12-05 00:29:03 -0500

    cosmos db

--- src/App.js
+++ src/App.js
@@ -10,7 +10,22 @@ function App() {
     })();
   });
 
-  return <div>{data}</div>;
+  return (<div>{data}
+  <h1>Static Web Apps Database Connections</h1>
+<blockquote>
+    Open the console in the browser developer tools to see the API responses.
+</blockquote>
+<div>
+    <button id="list" onclick="list()">List</button>
+    <button id="get" onclick="get()">Get</button>
+    <button id="update" onclick="update()">Update</button>
+    <button id="create" onclick="create()">Create</button>
+    <button id="delete" onclick="del()">Delete</button>
+</div>
+<script>
+    // add JavaScript here
+</script>
+  </div>);
 }
 
 export default App;
